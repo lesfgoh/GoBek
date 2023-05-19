c9f016c054fccdf97ea8c39de2aaabd96368ef74
itgrace <114488263+itmorgan@users.noreply.github.com>
2023-05-19 23:21:04 +0800

    Add files via upload

---
+++ supabase-py-develop/supabase-py-develop/tests/test_function_configuration.py
@@ -0,0 +1,15 @@
+import supabase
+
+
+def test_functions_client_initialization() -> None:
+    ref = "ooqqmozurnggtljmjkii"
+    url = f"https://{ref}.supabase.co"
+    # Sample JWT Key
+    key = "xxxxxxxxxxxxxx.xxxxxxxxxxxxxxx.xxxxxxxxxxxxxxx"
+    sp = supabase.Client(url, key)
+    sp.functions()
+    assert sp.functions_url == f"https://{ref}.functions.supabase.co"
+
+    url = "https://localhost:54322"
+    sp_local = supabase.Client(url, key)
+    assert sp_local.functions_url == f"{url}/functions/v1"
